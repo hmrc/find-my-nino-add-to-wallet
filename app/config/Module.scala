56001796f96d44f307ffb7459f1f13bab12f2566
Rishi Sachania <rishisachania@gmail.com>
2023-01-03 20:22:47 +0000

    bugfix for DI errors and default error handler

--- app/config/Module.scala
+++ app/config/Module.scala
@@ -17,11 +17,14 @@
 package config
 
 import com.google.inject.AbstractModule
+import uk.gov.hmrc.auth.core.AuthConnector
+import uk.gov.hmrc.play.bootstrap.auth.DefaultAuthConnector
 
 class Module extends AbstractModule {
 
   override def configure(): Unit = {
 
     bind(classOf[AppConfig]).asEagerSingleton()
+    bind(classOf[AuthConnector]).to(classOf[DefaultAuthConnector]).asEagerSingleton()
   }
 }
