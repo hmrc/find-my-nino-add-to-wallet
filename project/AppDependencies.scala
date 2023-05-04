ac6a88651eebe81a15218f26f604bdbcf5a1d247
Ali Mahmood <ali.mahmood@digital.hmrc.gov.uk>
2023-05-04 19:34:25 +0100

    Using shared version for mongo

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,11 @@ import sbt._
 
 object AppDependencies {
 
+  private val mongoVersion = "1.1.0"
+
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-backend-play-28"  % "7.1.0",
-    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "1.1.0",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % mongoVersion,
     "org.bouncycastle"        % "bcpkix-jdk18on"              % "1.71",
     "org.bouncycastle"        % "bcprov-jdk18on"              % "1.71",
     "org.bouncycastle"        % "bcutil-jdk18on"              % "1.71",
@@ -14,7 +16,7 @@ object AppDependencies {
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % "7.1.0"             % "test, it",
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"     % mongoVersion            % "test, it",
     "com.github.simplyscala"  % "scalatest-embedmongo_2.12"   % "0.2.4"             % "test",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"    % "0.71.0"            % Test,
     "com.vladsch.flexmark"    %  "flexmark-all"               % "0.36.8"            % "test, it",
