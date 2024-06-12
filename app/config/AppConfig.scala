4bf54cccdb9a09f6d11a2ed0f8e463e5bf3395e3
Rishi Sachania <rishisachania@gmail.com>
2024-06-12 11:19:28 +0100

    ITS-3576, removing gov-wallet relevant code (#91)

--- app/config/AppConfig.scala
+++ app/config/AppConfig.scala
@@ -34,23 +34,7 @@ class AppConfig @Inject()(config: Configuration) {
   val googleOrigins: String = config.get[String]("googlePass.origins")
   val cacheTtl: Int = config.get[Int]("mongodb.timeToLiveInSeconds")
 
-  val govukPassContext: List[String] = config.get[String]("govukpass.context").split(",").toList
-  val govukPassSub: String = config.get[String]("govukpass.sub")
-  val govukPassNbf: Int = config.get[Int]("govukpass.nbf")
-  val govukPassIss: String = config.get[String]("govukpass.iss")
-  val govukPassExp: Int = config.get[Int]("govukpass.exp")
-  val govukPassIat: Int = config.get[Int]("govukpass.iat")
-  val govukPassdefaultExpirationYears = config.get[Int]("govukpass.defaultExpirationYears")
-  val govukVerificatonPrivateKey = config.get[String]("govukpass.govukVerificatonPrivateKey")
-  val govukVerificatonPublicKeyX = config.get[String]("govukpass.govukVerificatonPublicKeyX")
-  val govukVerificatonPublicKeyY = config.get[String]("govukpass.govukVerificatonPublicKeyY")
-  val govukWalletJWTEncrypted = config.get[Boolean]("govukpass.govUkWalletJWTEncrypted")
-  val govukVerificatonPublicKeyID = config.get[String]("govukpass.govukVerificatonPublicKeyID")
-  val govukVerificatonPublicKeyIDPrefix = config.get[String]("govukpass.govukVerificatonPublicKeyIDPrefix")
 
-  lazy val govukWalletEnabled = config.getOptional[Boolean]("features.govuk-wallet-enabled").getOrElse(false)
-
-  val govukWalletUrl: String = config.get[String]("govukpass.govUkWalletUrl")
 
   val encryptionKey: String = config.get[String]("mongodb.encryption.key")
 
