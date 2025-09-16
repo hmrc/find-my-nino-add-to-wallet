a87723531e65d097d402224c2be24a37eda6a612
SandeepUmredkar <1963092+SandeepUmredkar@users.noreply.github.com>
2025-09-16 09:39:32 +0100

    DDCNL-11037

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,9 +2,9 @@ import sbt.*
 
 object AppDependencies {
 
-  private val mongoToggleVersion = "2.2.0"
+  private val mongoToggleVersion = "2.3.0"
   private val playVersion        = "play-30"
-  private val bootstrapVersion   = "10.0.0"
+  private val bootstrapVersion   = "10.1.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                  %% s"bootstrap-backend-$playVersion"            % bootstrapVersion,
@@ -12,16 +12,16 @@ object AppDependencies {
     "org.bouncycastle"              % "bcprov-jdk18on"                             % "1.81",
     "org.bouncycastle"              % "bcutil-jdk18on"                             % "1.81",
     "com.google.zxing"              % "core"                                       % "3.5.3",
-    "com.nimbusds"                  % "nimbus-jose-jwt"                            % "10.4",
-    "com.google.api-client"         % "google-api-client"                          % "2.8.0",
+    "com.nimbusds"                  % "nimbus-jose-jwt"                            % "10.5",
+    "com.google.api-client"         % "google-api-client"                          % "2.8.1",
     "org.typelevel"                %% "cats-core"                                  % "2.13.0",
-    "com.google.auth"               % "google-auth-library-oauth2-http"            % "1.37.1",
+    "com.google.auth"               % "google-auth-library-oauth2-http"            % "1.39.0",
     "com.google.guava"              % "guava"                                      % "33.4.8-jre",
     "com.auth0"                     % "java-jwt"                                   % "4.5.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala"                       % "2.19.2",
-    "uk.gov.hmrc"                  %% s"crypto-json-$playVersion"                  % "8.2.0",
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"                       % "2.20.0",
+    "uk.gov.hmrc"                  %% s"crypto-json-$playVersion"                  % "8.3.0",
     "uk.gov.hmrc"                  %% s"mongo-feature-toggles-client-$playVersion" % mongoToggleVersion,
-    "uk.gov.hmrc"                  %% "domain-play-30"                             % "12.1.0"
+    "uk.gov.hmrc"                  %% "domain-play-30"                             % "13.0.0"
   )
 
   val test: Seq[ModuleID] = Seq(
